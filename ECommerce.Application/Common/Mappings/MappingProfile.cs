48a89ce9680505ef916d7475f367d88b9bc0570d
olofs22 <olof.business.svensson@gmail.com>
2026-05-05 19:42:09 +0200

    work in progress application layer

---
+++ ECommerce.Application/Common/Mappings/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using ECommerce.Application.Products.DTOs;
+using ECommerce.Application.Products.Commands.CreateProduct;
+using ECommerce.Application.Products.Commands.UpdateProduct;
+using ECommerce.Application.Categories.DTOs;
+using ECommerce.Application.Categories.Commands.CreateCategory;
+using ECommerce.Application.Orders.DTOs;
+using ECommerce.Domain.Entities;
+
+namespace ECommerce.Application.Common.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // Product
+        CreateMap<Product, ProductDto>()
+            .ForMember(dest => dest.CategoryName,
+                opt => opt.MapFrom(src => src.Category != null ? src.Category.Name : string.Empty));
+        CreateMap<CreateProductCommand, Product>();
+        CreateMap<UpdateProductCommand, Product>();
+
+        // Category
+        CreateMap<Category, CategoryDto>();
+        CreateMap<CreateCategoryCommand, Category>();
+
+        // Order
+        CreateMap<Order, OrderDto>();
+        CreateMap<OrderItem, OrderItemDto>();
+    }
+}
